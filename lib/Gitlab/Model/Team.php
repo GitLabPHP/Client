5a4a6f8d17f8f4a79ed6aa18ace4ce768773a8c9
Matt Humphrey <matt@tomatto.co.uk>
2013-06-27 15:32:45 +0100

    Sorted missing parameters from Team model

--- lib/Gitlab/Model/Team.php
+++ lib/Gitlab/Model/Team.php
@@ -43,7 +43,7 @@ class Team extends AbstractModel
 
     public function members()
     {
-        $data = $this->api('teams')->members();
+        $data = $this->api('teams')->members($this->id);
 
         $members = array();
         foreach ($data as $member) {
@@ -55,28 +55,28 @@ class Team extends AbstractModel
 
     public function member($user_id)
     {
-        $data = $this->api('teams')->member($user_id);
+        $data = $this->api('teams')->member($this->id, $user_id);
 
         return User::fromArray($this->getClient(), $data);
     }
 
     public function addMember($user_id, $access_level)
     {
-        $data = $this->api('teams')->addMember($user_id, $access_level);
+        $data = $this->api('teams')->addMember($this->id, $user_id, $access_level);
 
         return User::fromArray($this->getClient(), $data);
     }
 
     public function removeMember($user_id)
     {
-        $this->api('teams')->removeMember($user_id);
+        $this->api('teams')->removeMember($this->id, $user_id);
 
         return true;
     }
 
     public function projects()
     {
-        $data = $this->api('teams')->projects();
+        $data = $this->api('teams')->projects($this->id);
 
         $projects = array();
         foreach ($data as $project) {
@@ -88,21 +88,21 @@ class Team extends AbstractModel
 
     public function project($project_id)
     {
-        $data = $this->api('teams')->project($project_id);
+        $data = $this->api('teams')->project($this->id, $project_id);
 
         return Project::fromArray($this->getClient(), $data);
     }
 
     public function addProject($project_id, $greatest_access_level)
     {
-        $data = $this->api('teams')->addProject($project_id, $greatest_access_level);
+        $data = $this->api('teams')->addProject($this->id, $project_id, $greatest_access_level);
 
         return Project::fromArray($this->getClient(), $data);
     }
 
     public function removeProject($project_id)
     {
-        $this->api('teams')->removeProject($project_id);
+        $this->api('teams')->removeProject($this->id, $project_id);
 
         return true;
     }
