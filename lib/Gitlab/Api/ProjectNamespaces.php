65dc67929188f26e2059e99deb56e9c5608232b5
Matt Humphrey <git@m4tt.co>
2015-07-28 12:27:19 +0100

    Added namespace search endpoint. Fixes #101

--- lib/Gitlab/Api/ProjectNamespaces.php
+++ lib/Gitlab/Api/ProjectNamespaces.php
@@ -14,4 +14,19 @@ class ProjectNamespaces extends AbstractApi
             'per_page' => $per_page
         ));
     }
+
+    /**
+     * @param string $terms
+     * @param int $page
+     * @param int $per_page
+     * @return mixed
+     */
+    public function search($terms, $page = 1, $per_page = self::PER_PAGE)
+    {
+        return $this->get('namespaces', array(
+            'search' => $terms,
+            'page' => $page,
+            'per_page' => $per_page
+        ));
+    }
 }
--- test/Gitlab/Tests/Api/ProjectNamespacesTest.php
+++ test/Gitlab/Tests/Api/ProjectNamespacesTest.php
@@ -43,6 +43,25 @@ class ProjectNamespacesTest extends ApiTestCase
 
         $this->assertEquals($expectedArray, $api->all());
     }
+    /**
+     * @test
+     */
+    public function shouldSearchNamespaces()
+    {
+        $expectedArray = array(
+            array('id' => 1, 'name' => 'bespokes'),
+            array('id' => 2, 'name' => 'internal')
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('namespaces', array('search' => 'term', 'page' => 1, 'per_page' => 10))
+            ->will($this->returnValue($expectedArray))
+        ;
+
+        $this->assertEquals($expectedArray, $api->search('term', 1, 10));
+    }
 
     protected function getApiClass()
     {
