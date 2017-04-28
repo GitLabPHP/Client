dba7da61203595cc6d3161dcc9e1b918310b5106
cizel <i@cizel.cn>
2017-04-28 15:58:13 +0800

    Add tags API

---
+++ test/Gitlab/Tests/Api/TagsTest.php
@@ -0,0 +1,87 @@
+<?php namespace Gitlab\Tests\Api;
+
+use Gitlab\Api\AbstractApi;
+
+class TagsTest extends ApiTestCase
+{
+    /**
+     * @test
+     */
+    public function shouldGetAllTags()
+    {
+        $expectedArray = array(
+            array('name' => 'v1.0.0'),
+            array('name' => 'v1.1.0'),
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('projects/1/repository/tags')
+            ->will($this->returnValue($expectedArray));
+        $this->assertEquals($expectedArray, $api->all(1));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldShowTag()
+    {
+        $expectedArray = array(
+            array('name' => 'v1.0.0'),
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('projects/1/repository/tags/v1.0.0')
+            ->will($this->returnValue($expectedArray));
+        $this->assertEquals($expectedArray, $api->show(1, 'v1.0.0'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateTag()
+    {
+        $expectedArray = array(
+            array('name' => 'v1.1.0'),
+        );
+
+        $params = array(
+            'id'       => 1,
+            'tag_name' => 'v1.1.0',
+            'ref'      => 'ref/heads/master'
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('post')
+            ->with('projects/1/repository/tags', $params)
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->create(1, $params));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRemoveTag()
+    {
+        $expectedArray = array(
+            array('name' => 'v1.1.0'),
+        );
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('delete')
+            ->with('projects/1/repository/tags/v1.1.0')
+            ->will($this->returnValue($expectedArray));
+        $this->assertEquals($expectedArray, $api->remove(1, 'v1.1.0'));
+    }
+
+    protected function getApiClass()
+    {
+        return 'Gitlab\Api\Tags';
+    }
+}
