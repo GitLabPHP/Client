0240533b5d5d8af16b811aefb9a7803fb3669574
Daniel André Eikeland <daniel@cloudinsurance.io>
2017-11-17 13:37:34 +0100

    Formatting fixes

--- lib/Gitlab/Api/Deployments.php
+++ lib/Gitlab/Api/Deployments.php
@@ -2,22 +2,22 @@
 
 class Deployments extends AbstractApi
 {
-    /**
-     * @param int $project_id
-     * @return mixed
-     */
-    public function all($project_id)
-    {
-        return $this->get($this->getProjectPath($project_id, 'deployments'));
-    }
+	/**
+	 * @param int $project_id
+	 * @return mixed
+	 */
+	public function all($project_id)
+	{
+		return $this->get($this->getProjectPath($project_id, 'deployments'));
+	}
 
-    /**
-     * @param int $project_id
-     * @param string $deployment_id
-     * @return mixed
-     */
-    public function show($project_id, $deployment_id)
-    {
-        return $this->get($this->getProjectPath($project_id, 'deployments/'.$deployment_id));
-    }
+	/**
+	 * @param int $project_id
+	 * @param string $deployment_id
+	 * @return mixed
+	 */
+	public function show($project_id, $deployment_id)
+	{
+		return $this->get($this->getProjectPath($project_id, 'deployments/' . $deployment_id));
+	}
 }
