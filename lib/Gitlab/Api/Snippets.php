7cc640bdb0629aaf4a409b0ffb1cd0881e8aa484
Mirko Schmidt <mirko.schmidt@jtl-software.com>
2018-02-14 13:54:00 +0100

    Update PHPDocs and add missing required param visibility to model project in createSnippet method

--- lib/Gitlab/Api/Snippets.php
+++ lib/Gitlab/Api/Snippets.php
@@ -26,6 +26,7 @@ class Snippets extends AbstractApi
      * @param string $title
      * @param string $filename
      * @param string $code
+     * @param $visibility
      * @return mixed
      */
     public function create($project_id, $title, $filename, $code, $visibility)
