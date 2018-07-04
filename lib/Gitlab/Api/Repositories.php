8e441b5f45bd42e58162698a68cca103705dc6cd
mlhess <mlhess@24mac24.local>
2018-07-04 18:15:00 -0400

    add a method to pull refs (tags and branches) for a given commit

--- lib/Gitlab/Api/Repositories.php
+++ lib/Gitlab/Api/Repositories.php
@@ -178,6 +178,16 @@ class Repositories extends AbstractApi
 
     /**
      * @param int $project_id
+     * @param $sha
+     * @return mixed
+     */
+    public function commitrefs($project_id, $sha)
+    {
+        return $this->get($this->getProjectPath($project_id, 'repository/commits/'.$this->encodePath($sha) . '/refs'));
+    }
+
+    /**
+     * @param int $project_id
      * @param array $parameters (
      *
      *     @var string $branch         Name of the branch to commit into. To create a new branch, also provide start_branch.
