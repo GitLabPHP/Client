eb65517936b52ba8a54e11a56384a77ad424a7e3
bmalex88 <bmalex88@gmail.com>
2018-11-08 07:52:16 +0200

    Tag message and release properties

---
+++ lib/Gitlab/Model/Release.php
@@ -0,0 +1,43 @@
+<?php namespace Gitlab\Model;
+
+use Gitlab\Client;
+
+/**
+ * Class Release
+ *
+ * @property-read string $tag_name
+ * @property-read string $description
+ * @property-read Commit $commit
+ */
+class Release extends AbstractModel
+{
+    /**
+     * @var array
+     */
+    protected static $properties = array(
+        'tag_name',
+        'description'
+    );
+
+    /**
+     * @param Client  $client
+     * @param array   $data
+     * @return Release
+     */
+    public static function fromArray(Client $client, array $data)
+    {
+        $release = new static($client);
+
+        return $release->hydrate($data);
+    }
+
+    /**
+     * @param Project $project
+     * @param int $id
+     * @param Client  $client
+     */
+    public function __construct(Client $client = null)
+    {
+        $this->setClient($client);
+    }
+}
