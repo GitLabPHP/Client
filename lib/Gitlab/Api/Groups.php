d2a2219ffbf5ce3ec62185f171b6fc14b36b93b6
Unai Tolosa Pontesta <utolosa002@gmail.com>
2019-01-29 01:11:28 +0100

    Update Groups.php
    changing whitespaces to tests

--- lib/Gitlab/Api/Groups.php
+++ lib/Gitlab/Api/Groups.php
@@ -207,10 +207,10 @@ class Groups extends AbstractApi
         $projects= $this->get('groups/'.$this->encodePath($id).'/projects');
         $allprojects=$projects;
         $page=1;
-	while ($projects!=null){
-		$page++;
-		$projects= $this->get('groups/'.$this->encodePath($id).'/projects?page='.$page);
-		$allprojects=array_merge($allprojects,$projects);
+        while ($projects!=null){
+                $page++;
+                $projects= $this->get('groups/'.$this->encodePath($id).'/projects?page='.$page);
+                $allprojects=array_merge($allprojects,$projects);
         }
         return $allprojects;
     }
