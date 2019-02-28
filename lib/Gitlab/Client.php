d24b8b3aef1f1551d6cd779b74366bda23c11af1
Miriam Gil <gil.miriam.deljulia@gmail.com>
2019-02-28 12:15:22 +0100

    fix test

--- lib/Gitlab/Client.php
+++ lib/Gitlab/Client.php
@@ -84,7 +84,7 @@ class Client
         $this->httpClientBuilder->addPlugin(new HistoryPlugin($this->responseHistory));
         $this->httpClientBuilder->addPlugin(new ApiVersion());
         $this->httpClientBuilder->addPlugin(new HeaderDefaultsPlugin([
-            'User-Agent' => 'php1-gitlab-api (http://github.com/m4tthumphrey/php1-gitlab-api)',
+            'User-Agent' => 'php-gitlab-api (http://github.com/m4tthumphrey/php-gitlab-api)',
         ]));
         $this->httpClientBuilder->addPlugin(new RedirectPlugin());
 
