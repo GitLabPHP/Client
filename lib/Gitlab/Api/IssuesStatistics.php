dd159b000bae16eb0aca680a2c11e8db61bd2e88
Bertrand Jamin <bertrand@lepermislibre.fr>
2019-09-25 23:14:46 +0200

    Add routes to retrieve issues statistics

---
+++ lib/Gitlab/Api/IssuesStatistics.php
@@ -0,0 +1,84 @@
+<?php namespace Gitlab\Api;
+
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IssuesStatistics extends AbstractApi
+{
+    public function all($parameters)
+    {
+        return $this->get('issues_statistics', $this->createOptionsResolver()->resolve($parameters));
+    }
+
+    public function project($project_id, $parameters)
+    {
+        return $this->get($this->getProjectPath($project_id, 'issues_statistics'), $this->createOptionsResolver()->resolve($parameters));
+    }
+
+    public function group($group_id, $parameters)
+    {
+        return $this->get($this->getGroupPath($group_id, 'issues_statistics'), $this->createOptionsResolver()->resolve($parameters));
+    }
+
+    protected function createOptionsResolver()
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver->setDefined('milestone')
+            ->setAllowedTypes('milestone', 'string');
+
+        $resolver->setDefined('labels')
+            ->setAllowedTypes('labels', 'string');
+
+        $resolver->setDefined('scope')
+            ->setAllowedValues('scope', ['created-by-me', 'assigned-to-me', 'all']);
+
+        $resolver->setDefined('author_id')
+            ->setAllowedTypes('author_id', 'integer');
+
+        $resolver->setDefined('author_username')
+            ->setAllowedTypes('author_username', 'string');
+
+        $resolver->setDefined('assignee_id')
+            ->setAllowedTypes('assignee_id', 'integer');
+
+        $resolver->setDefined('assignee_username')
+            ->setAllowedTypes('assignee_username', 'string');
+
+        $resolver->setDefined('my_reaction_emoji')
+            ->setAllowedTypes('my_reaction_emoji', 'string');
+
+        $resolver->setDefined('search')
+            ->setAllowedTypes('search', 'string');
+
+        $datetimeNormalizer = function (Options $resolver, \DateTimeInterface $value) {
+            return $value->format('c');
+        };
+
+        $resolver->setDefined('created_after')
+            ->setAllowedTypes('created_after', \DateTimeInterface::class)
+            ->setNormalizer('created_after', $datetimeNormalizer);
+
+        $resolver->setDefined('created_before')
+            ->setAllowedTypes('created_before', \DateTimeInterface::class)
+            ->setNormalizer('created_before', $datetimeNormalizer);
+
+        $resolver->setDefined('updated_after')
+            ->setAllowedTypes('updated_after', \DateTimeInterface::class)
+            ->setNormalizer('updated_after', $datetimeNormalizer);
+
+        $resolver->setDefined('updated_before')
+            ->setAllowedTypes('updated_before', \DateTimeInterface::class)
+            ->setNormalizer('updated_before', $datetimeNormalizer);
+
+        $booleanNormalizer = function (Options $resolver, $value) {
+            return $value ? 'true' : 'false';
+        };
+
+        $resolver->setDefined('confidential')
+            ->setAllowedTypes('confidential', 'bool')
+            ->setNormalizer('confidential', $booleanNormalizer);
+
+        return $resolver;
+    }
+}
---
+++ test/Gitlab/Tests/Api/IssuesStatisticsTest.php
@@ -0,0 +1,92 @@
+<?php namespace Gitlab\Tests\Api;
+
+use Gitlab\Api\IssuesStatistics;
+
+class IssuesStatisticsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldGetAll()
+    {
+        $expectedArray = array();
+
+        $now = new \DateTime();
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('issues_statistics', [
+                'milestone' => '',
+                'labels' => '',
+                'scope' => 'created-by-me',
+                'author_id' => 1,
+                'author_username' => '',
+                'assignee_id' => 1,
+                'assignee_username' => '',
+                'my_reaction_emoji' => '',
+                'search' => '',
+                'created_after' => $now->format('c'),
+                'created_before' => $now->format('c'),
+                'updated_after' => $now->format('c'),
+                'updated_before' => $now->format('c'),
+                'confidential' => 'false'
+            ])
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->all([
+            'milestone' => '',
+            'labels' => '',
+            'scope' => 'created-by-me',
+            'author_id' => 1,
+            'author_username' => '',
+            'assignee_id' => 1,
+            'assignee_username' => '',
+            'my_reaction_emoji' => '',
+            'search' => '',
+            'created_after' => $now,
+            'created_before' => $now,
+            'updated_after' => $now,
+            'updated_before' => $now,
+            'confidential' => false
+        ]));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetProject()
+    {
+        $expectedArray = array();
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('projects/1/issues_statistics', [])
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->project(1, []));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetGroup()
+    {
+        $expectedArray = array();
+
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('groups/1/issues_statistics', [])
+            ->will($this->returnValue($expectedArray));
+
+        $this->assertEquals($expectedArray, $api->group(1, []));
+    }
+
+
+    protected function getApiClass()
+    {
+        return IssuesStatistics::class;
+    }
+}
