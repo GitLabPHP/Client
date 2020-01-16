c2a9d3e8cad8ef26cbf194e16a10851a6e3c56bd
Cliff <atcliffunderline@gmail.com>
2020-01-16 15:05:23 +0300

    Adding 'stats' property to Commit model

--- lib/Gitlab/Model/Commit.php
+++ lib/Gitlab/Model/Commit.php
@@ -39,7 +39,8 @@ class Commit extends AbstractModel
         'authored_date',
         'committed_date',
         'created_at',
-        'project'
+        'project',
+        'stats'
     );
 
     /**
