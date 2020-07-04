3e06157fd0c39e1504fadccbddc56f280b33d4ad
Graham Campbell <graham@alt-three.com>
2020-07-04 21:25:14 +0100

    Made response mediator final

--- lib/Gitlab/HttpClient/Message/ResponseMediator.php
+++ lib/Gitlab/HttpClient/Message/ResponseMediator.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ResponseInterface;
 /**
  * Utilities to parse response headers and content.
  */
-class ResponseMediator
+final class ResponseMediator
 {
     /**
      * Return the response body as a string or json array if content type is application/json.
