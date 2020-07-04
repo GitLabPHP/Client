9d04d89684aac2e4ce2ec56fafd73eac63a6cef3
Graham Campbell <graham@alt-three.com>
2020-07-04 17:27:01 +0100

    Marked unused exceptions as deprecated

--- lib/Gitlab/Exception/ValidationFailedException.php
+++ lib/Gitlab/Exception/ValidationFailedException.php
@@ -6,6 +6,8 @@ namespace Gitlab\Exception;
  * ValidationFailedException.
  *
  * @author Joseph Bielawski <stloyd@gmail.com>
+ *
+ * @derpecated since version 9.18 and will be removed in 10.0.
  */
 class ValidationFailedException extends ErrorException implements ExceptionInterface
 {
