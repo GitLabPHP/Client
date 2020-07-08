c4217622a06d51a840db48a97182ef6b3c638694
Graham Campbell <graham@alt-three.com>
2020-07-08 16:23:05 +0100

    Revert marking builder as soft final

--- lib/Gitlab/HttpClient/Builder.php
+++ lib/Gitlab/HttpClient/Builder.php
@@ -20,8 +20,6 @@ use Psr\Cache\CacheItemPoolInterface;
  *
  * This will allow you to fluently add and remove plugins.
  *
- * @final
- *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  * @author Graham Campbell <graham@alt-three.com>
  */
