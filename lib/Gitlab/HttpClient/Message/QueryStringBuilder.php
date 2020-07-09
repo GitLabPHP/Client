d938a67144260105c44de60f8f323a7a877aa387
Graham Campbell <graham@alt-three.com>
2020-07-09 19:04:20 +0100

    Restored old QueryStringBuilder and mark as deprecated

---
+++ lib/Gitlab/HttpClient/Message/QueryStringBuilder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gitlab\HttpClient\Message;
+
+use Gitlab\HttpClient\Util\QueryStringBuilder as UtilQueryStringBuilder;
+
+/**
+ * @deprecated since 9.18 and will be removed in 10.0.
+ */
+final class QueryStringBuilder
+{
+    /**
+     * Encode a query as a query string according to RFC 3986. Indexed arrays are encoded using
+     * empty squared brackets ([]) unlike http_build_query.
+     *
+     * @param mixed $query
+     *
+     * @return string
+     *
+     * @deprecated since 9.18 and will be removed in 10.0.
+     */
+    public static function build($query)
+    {
+        @trigger_error(sprintf('The %s() method is deprecated since version 9.18 and will be removed in 10.0.', __METHOD__), E_USER_DEPRECATED);
+
+        return UtilQueryStringBuilder::build($query);
+    }
+
+    /**
+     * Tell if the given array is an indexed one (i.e. contains only sequential integer keys starting from 0).
+     *
+     * @param array $query
+     *
+     * @return bool
+     *
+     * @deprecated since 9.18 and will be removed in 10.0.
+     */
+    public static function isIndexedArray(array $query)
+    {
+        @trigger_error(sprintf('The %s() method is deprecated since version 9.18 and will be removed in 10.0.', __METHOD__), E_USER_DEPRECATED);
+
+        if (0 === count($query) || !isset($query[0])) {
+            return false;
+        }
+
+        return array_keys($query) === range(0, count($query) - 1);
+    }
+}
