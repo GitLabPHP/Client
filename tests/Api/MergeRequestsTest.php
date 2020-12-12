231014aad0e5b6eaff7258508496c4ee572c8e41
Graham Campbell <graham@alt-three.com>
2020-12-12 16:08:03 +0000

    Updated copyright information

--- tests/Api/MergeRequestsTest.php
+++ tests/Api/MergeRequestsTest.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Gitlab API library.
+ *
+ * (c) Matt Humphrey <matth@windsor-telecom.co.uk>
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gitlab\Tests\Api;
 
 use Gitlab\Api\MergeRequests;
