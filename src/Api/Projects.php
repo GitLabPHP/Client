31d6f7027391aaad2f3be2057a2a881808627618
Aad Mathijssen <aad.mathijssen@isaac.nl>
2021-02-10 17:23:42 +0100

    Align entries in PHPDoc

--- src/Api/Projects.php
+++ src/Api/Projects.php
@@ -24,31 +24,31 @@ class Projects extends AbstractApi
     /**
      * @param array $parameters {
      *
-     *     @var bool   $archived                    limit by archived status
-     *     @var string $visibility                  limit by visibility public, internal, or private
-     *     @var string $order_by                    Return projects ordered by id, name, path, created_at, updated_at,
-     *                                              last_activity_at, repository_size, storage_size, packages_size or
-     *                                              wiki_size fields (default is created_at)
-     *     @var string $sort                        Return projects sorted in asc or desc order (default is desc)
-     *     @var string $search                      return list of projects matching the search criteria
-     *     @var bool   $search_namespaces           Include ancestor namespaces when matching search criteria
-     *     @var bool   $simple                      return only the ID, URL, name, and path of each project
-     *     @var bool   $owned                       limit by projects owned by the current user
-     *     @var bool   $membership                  limit by projects that the current user is a member of
-     *     @var bool   $starred                     limit by projects starred by the current user
-     *     @var bool   $statistics                  include project statistics
-     *     @var bool   $with_issues_enabled         limit by enabled issues feature
-     *     @var bool   $with_merge_requests_enabled limit by enabled merge requests feature
-     *     @var int    $min_access_level            Limit by current user minimal access level
-     *     @var int    $id_after                    Limit by project id's greater than the specified id
-     *     @var int    $id_before                   Limit by project id's less than the specified id
-     *     @var \DateTimeInterface $last_activity_after  Limit by last_activity after specified time
-     *     @var \DateTimeInterface $last_activity_before Limit by last_activity before specified time
-     *     @var bool   $repository_checksum_failed  Limit by failed repository checksum calculation
-     *     @var string $repository_storage          Limit by repository storage type
-     *     @var bool   $wiki_checksum_failed        Limit by failed wiki checksum calculation
-     *     @var bool   $with_custom_attributes      Include custom attributes in response
-     *     @var string $with_programming_language   Limit by programming language
+     *     @var bool               $archived                    limit by archived status
+     *     @var string             $visibility                  limit by visibility public, internal, or private
+     *     @var string             $order_by                    Return projects ordered by id, name, path, created_at, updated_at,
+     *                                                          last_activity_at, repository_size, storage_size, packages_size or
+     *                                                          wiki_size fields (default is created_at)
+     *     @var string             $sort                        Return projects sorted in asc or desc order (default is desc)
+     *     @var string             $search                      return list of projects matching the search criteria
+     *     @var bool               $search_namespaces           Include ancestor namespaces when matching search criteria
+     *     @var bool               $simple                      return only the ID, URL, name, and path of each project
+     *     @var bool               $owned                       limit by projects owned by the current user
+     *     @var bool               $membership                  limit by projects that the current user is a member of
+     *     @var bool               $starred                     limit by projects starred by the current user
+     *     @var bool               $statistics                  include project statistics
+     *     @var bool               $with_issues_enabled         limit by enabled issues feature
+     *     @var bool               $with_merge_requests_enabled limit by enabled merge requests feature
+     *     @var int                $min_access_level            Limit by current user minimal access level
+     *     @var int                $id_after                    Limit by project id's greater than the specified id
+     *     @var int                $id_before                   Limit by project id's less than the specified id
+     *     @var \DateTimeInterface $last_activity_after         Limit by last_activity after specified time
+     *     @var \DateTimeInterface $last_activity_before        Limit by last_activity before specified time
+     *     @var bool               $repository_checksum_failed  Limit by failed repository checksum calculation
+     *     @var string             $repository_storage          Limit by repository storage type
+     *     @var bool               $wiki_checksum_failed        Limit by failed wiki checksum calculation
+     *     @var bool               $with_custom_attributes      Include custom attributes in response
+     *     @var string             $with_programming_language   Limit by programming language
      * }
      *
      * @throws UndefinedOptionsException If an option name is undefined
