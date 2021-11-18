a5b350646508437367a3298682954429006feaf8
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2021-11-18 17:42:22 +0100

    refactor: apply style cli diff

--- tests/Api/ResourceIterationEventsTest.php
+++ tests/Api/ResourceIterationEventsTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Gitlab\Tests\Api;
 
-
 use Gitlab\Api\ResourceIterationEvents;
 
 class ResourceIterationEventsTest extends TestCase
@@ -16,58 +15,58 @@ class ResourceIterationEventsTest extends TestCase
     {
         $expectedArray = [
             [
-                "id" => 142,
-                "user" => [
-                    "id" => 1,
-                    "name" => "Administrator",
-                    "username" => "root",
-                    "state" => "active",
-                    "avatar_url" => "https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
-                    "web_url" => "http=>//gitlab.example.com/root",
+                'id' => 142,
+                'user' => [
+                    'id' => 1,
+                    'name' => 'Administrator',
+                    'username' => 'root',
+                    'state' => 'active',
+                    'avatar_url' => 'https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+                    'web_url' => 'http=>//gitlab.example.com/root',
                 ],
-                "created_at" => "2018-08-20T13=>38=>20.077Z",
-                "resource_type" => "Issue",
-                "resource_id" => 253,
-                "iteration" => [
-                    "id" => 50,
-                    "iid" => 9,
-                    "group_id" => 5,
-                    "title" => "Iteration I",
-                    "description" => "Ipsum Lorem",
-                    "state" => 1,
-                    "created_at" => "2020-01-27T05=>07=>12.573Z",
-                    "updated_at" => "2020-01-27T05=>07=>12.573Z",
-                    "due_date" => null,
-                    "start_date" => null,
+                'created_at' => '2018-08-20T13=>38=>20.077Z',
+                'resource_type' => 'Issue',
+                'resource_id' => 253,
+                'iteration' => [
+                    'id' => 50,
+                    'iid' => 9,
+                    'group_id' => 5,
+                    'title' => 'Iteration I',
+                    'description' => 'Ipsum Lorem',
+                    'state' => 1,
+                    'created_at' => '2020-01-27T05=>07=>12.573Z',
+                    'updated_at' => '2020-01-27T05=>07=>12.573Z',
+                    'due_date' => null,
+                    'start_date' => null,
                 ],
-                "action" => "add",
+                'action' => 'add',
             ],
             [
-                "id" => 143,
-                "user" => [
-                    "id" => 1,
-                    "name" => "Administrator",
-                    "username" => "root",
-                    "state" => "active",
-                    "avatar_url" => "https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
-                    "web_url" => "http=>//gitlab.example.com/root",
+                'id' => 143,
+                'user' => [
+                    'id' => 1,
+                    'name' => 'Administrator',
+                    'username' => 'root',
+                    'state' => 'active',
+                    'avatar_url' => 'https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+                    'web_url' => 'http=>//gitlab.example.com/root',
                 ],
-                "created_at" => "2018-08-21T14=>38=>20.077Z",
-                "resource_type" => "Issue",
-                "resource_id" => 253,
-                "iteration" => [
-                    "id" => 53,
-                    "iid" => 13,
-                    "group_id" => 5,
-                    "title" => "Iteration II",
-                    "description" => "Ipsum Lorem ipsum",
-                    "state" => 2,
-                    "created_at" => "2020-01-27T05=>07=>12.573Z",
-                    "updated_at" => "2020-01-27T05=>07=>12.573Z",
-                    "due_date" => null,
-                    "start_date" => null,
+                'created_at' => '2018-08-21T14=>38=>20.077Z',
+                'resource_type' => 'Issue',
+                'resource_id' => 253,
+                'iteration' => [
+                    'id' => 53,
+                    'iid' => 13,
+                    'group_id' => 5,
+                    'title' => 'Iteration II',
+                    'description' => 'Ipsum Lorem ipsum',
+                    'state' => 2,
+                    'created_at' => '2020-01-27T05=>07=>12.573Z',
+                    'updated_at' => '2020-01-27T05=>07=>12.573Z',
+                    'due_date' => null,
+                    'start_date' => null,
                 ],
-                "action" => "remove",
+                'action' => 'remove',
             ],
         ];
 
@@ -80,38 +79,37 @@ class ResourceIterationEventsTest extends TestCase
         $this->assertEquals($expectedArray, $api->all(1, 253));
     }
 
-
     /**
      * @test
      */
     public function shouldShowEvent(): void
     {
         $expectedArray = [
-            "id" => 142,
-            "user" => [
-                "id" => 1,
-                "name" => "Administrator",
-                "username" => "root",
-                "state" => "active",
-                "avatar_url" => "https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
-                "web_url" => "http=>//gitlab.example.com/root",
+            'id' => 142,
+            'user' => [
+                'id' => 1,
+                'name' => 'Administrator',
+                'username' => 'root',
+                'state' => 'active',
+                'avatar_url' => 'https=>//www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+                'web_url' => 'http=>//gitlab.example.com/root',
             ],
-            "created_at" => "2018-08-20T13=>38=>20.077Z",
-            "resource_type" => "Issue",
-            "resource_id" => 253,
-            "iteration" => [
-                "id" => 50,
-                "iid" => 9,
-                "group_id" => 5,
-                "title" => "Iteration I",
-                "description" => "Ipsum Lorem",
-                "state" => 1,
-                "created_at" => "2020-01-27T05=>07=>12.573Z",
-                "updated_at" => "2020-01-27T05=>07=>12.573Z",
-                "due_date" => null,
-                "start_date" => null,
+            'created_at' => '2018-08-20T13=>38=>20.077Z',
+            'resource_type' => 'Issue',
+            'resource_id' => 253,
+            'iteration' => [
+                'id' => 50,
+                'iid' => 9,
+                'group_id' => 5,
+                'title' => 'Iteration I',
+                'description' => 'Ipsum Lorem',
+                'state' => 1,
+                'created_at' => '2020-01-27T05=>07=>12.573Z',
+                'updated_at' => '2020-01-27T05=>07=>12.573Z',
+                'due_date' => null,
+                'start_date' => null,
             ],
-            "action" => "add",
+            'action' => 'add',
         ];
 
         $api = $this->getApiMock();
