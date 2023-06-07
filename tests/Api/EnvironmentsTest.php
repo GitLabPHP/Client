067061ede87032510bd449f4359e702aa0be0a79
Jan Fejtek <jan.fejtek@gmail.com>
2023-06-08 00:23:04 +0200

    Update EnvironmentsTest.php

--- tests/Api/EnvironmentsTest.php
+++ tests/Api/EnvironmentsTest.php
@@ -155,14 +155,14 @@ See merge request !1',
                 'name' => 'review/fix-baz',
                 'slug' => 'review-fix-baz-dfjre5',
                 'external_url' => 'https://review-fix-baz-dfjre5.example.gitlab.com',
-                'tier' => 'production'
+                'tier' => 'production',
             ],
         ];
 
         $params = [
             'name' => 'review/fix-baz',
             'external_url' => 'https://review-fix-baz-dfjre5.example.gitlab.com',
-            'tier' => 'production'
+            'tier' => 'production',
         ];
 
         $api = $this->getApiMock();
