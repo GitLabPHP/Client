5df6a1fc8ef73ceed5bc8cb85304cd54b6c0e622
Jeroen van den Enden <info@endroid.nl>
2023-08-16 18:02:53 +0000

    feature: update protected branches (via patch)

--- src/Api/Projects.php
+++ src/Api/Projects.php
@@ -1519,6 +1519,18 @@ class Projects extends AbstractApi
 
     /**
      * @param int|string $project_id
+     * @param string     $branch_name
+     * @param array      $parameters
+     *
+     * @return mixed
+     */
+    public function updateProtectedBranch($project_id, string $branch_name, array $parameters = [])
+    {
+        return $this->patch($this->getProjectPath($project_id, 'protected_branches/'.self::encodePath($branch_name)), $parameters);
+    }
+
+    /**
+     * @param int|string $project_id
      *
      * @return mixed
      */
