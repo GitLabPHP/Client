71d1408e488e1df53001bf14c9f7c63db2b3cd84
Luke40172 <luuk.kleiweg@gmail.com>
2023-10-08 16:12:26 +0200

    Added method to update approval configuration (#770)
    Co-authored-by: Luuk Kleiweg <luuk.kleiweg@gasa.org>

--- src/Api/Projects.php
+++ src/Api/Projects.php
@@ -1570,6 +1570,17 @@ class Projects extends AbstractApi
 
     /**
      * @param int|string $project_id
+     * @param array      $parameters
+     *
+     * @return mixed
+     */
+    public function updateApprovalsConfiguration($project_id, array $parameters = [])
+    {
+        return $this->post('projects/'.self::encodePath($project_id).'/approvals', $parameters);
+    }
+
+    /**
+     * @param int|string $project_id
      *
      * @return mixed
      */
