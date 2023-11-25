406a760ca4cee5c00e9106bef0622bf5bc0c9b0e
Wenchao Xue <wxue@paystone.com>
2023-11-25 13:21:25 -0500

    accept the parameters updated_before and updated_after (Introduced in GitLab 15.10)

--- src/Api/GroupsMilestones.php
+++ src/Api/GroupsMilestones.php
@@ -33,6 +33,10 @@ class GroupsMilestones extends AbstractApi
      *     @var int[]  $iids   return only the milestones having the given iids
      *     @var string $state  return only active or closed milestones
      *     @var string $search Return only milestones with a title or description matching the provided string.
+     *     @var string $updated_before Return only milestones updated before the given datetime.
+     *                                 Expected in ISO 8601 format (2019-03-15T08:00:00Z). Introduced in GitLab 15.10
+     *     @var string $updated_after  Return only milestones updated after the given datetime.
+     *                                 Expected in ISO 8601 format (2019-03-15T08:00:00Z). Introduced in GitLab 15.10
      * }
      *
      * @return mixed
@@ -50,6 +54,8 @@ class GroupsMilestones extends AbstractApi
             ->setAllowedValues('state', [self::STATE_ACTIVE, self::STATE_CLOSED])
         ;
         $resolver->setDefined('search');
+        $resolver->setDefined('updated_before');
+        $resolver->setDefined('updated_after');
 
         return $this->get('groups/'.self::encodePath($group_id).'/milestones', $resolver->resolve($parameters));
     }
