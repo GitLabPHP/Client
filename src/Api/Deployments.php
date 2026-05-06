52427db7d0ebca3ab771a144034ca01db0fbd53e
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2026-05-06 02:10:06 +0100

    Add deployment date filters (#868)
    Co-authored-by: Chris Mitchell <2536603+chumanfu@users.noreply.github.com>

--- src/Api/Deployments.php
+++ src/Api/Deployments.php
@@ -14,34 +14,61 @@ declare(strict_types=1);
 
 namespace Gitlab\Api;
 
+use Symfony\Component\OptionsResolver\Options;
+
 class Deployments extends AbstractApi
 {
     /**
      * @param array      $parameters {
      *
-     *     @var string $order_by                    Return deployments ordered by id, iid, created_at, updated_at,
-     *                                              or ref fields (default is id)
-     *     @var string $sort                        Return deployments sorted in asc or desc order (default is desc)
-     *     @var string $status                      Return deployments filtered by status of deployment allowed
-     *                                              values of status are 'created', 'running', 'success', 'failed',
-     *                                              'canceled', 'blocked'
-     *     @var string $environment                 Return deployments filtered to a particular environment
+     *     @var string             $order_by        Return deployments ordered by id, iid, created_at, updated_at, finished_at, or ref fields (default is id)
+     *     @var string             $sort            Return deployments sorted in asc or desc order (default is asc)
+     *     @var \DateTimeInterface $updated_after   Return deployments updated after the specified date
+     *     @var \DateTimeInterface $updated_before  Return deployments updated before the specified date
+     *     @var \DateTimeInterface $finished_after  Return deployments finished after the specified date. Use order_by finished_at and status success.
+     *     @var \DateTimeInterface $finished_before Return deployments finished before the specified date. Use order_by finished_at and status success.
+     *     @var string             $status          Return deployments filtered by status: created, running, success, failed, canceled, or blocked
+     *     @var string             $environment     Return deployments filtered to a particular environment
      * }
      */
     public function all(int|string $project_id, array $parameters = []): mixed
     {
         $resolver = $this->createOptionsResolver();
+        $datetimeNormalizer = function (Options $resolver, \DateTimeInterface $value): string {
+            return $value->format('c');
+        };
+
         $resolver->setDefined('order_by')
             ->setAllowedTypes('order_by', 'string')
-            ->setAllowedValues('order_by', ['id', 'iid', 'created_at', 'updated_at', 'ref']);
+            ->setAllowedValues('order_by', ['id', 'iid', 'created_at', 'updated_at', 'finished_at', 'ref'])
+        ;
         $resolver->setDefined('sort')
             ->setAllowedTypes('sort', 'string')
-            ->setAllowedValues('sort', ['desc', 'asc']);
+            ->setAllowedValues('sort', ['asc', 'desc'])
+        ;
+        $resolver->setDefined('updated_after')
+            ->setAllowedTypes('updated_after', \DateTimeInterface::class)
+            ->setNormalizer('updated_after', $datetimeNormalizer)
+        ;
+        $resolver->setDefined('updated_before')
+            ->setAllowedTypes('updated_before', \DateTimeInterface::class)
+            ->setNormalizer('updated_before', $datetimeNormalizer)
+        ;
+        $resolver->setDefined('finished_after')
+            ->setAllowedTypes('finished_after', \DateTimeInterface::class)
+            ->setNormalizer('finished_after', $datetimeNormalizer)
+        ;
+        $resolver->setDefined('finished_before')
+            ->setAllowedTypes('finished_before', \DateTimeInterface::class)
+            ->setNormalizer('finished_before', $datetimeNormalizer)
+        ;
         $resolver->setDefined('status')
             ->setAllowedTypes('status', 'string')
-            ->setAllowedValues('status', ['created', 'running', 'success', 'failed', 'canceled', 'blocked']);
+            ->setAllowedValues('status', ['created', 'running', 'success', 'failed', 'canceled', 'blocked'])
+        ;
         $resolver->setDefined('environment')
-            ->setAllowedTypes('environment', 'string');
+            ->setAllowedTypes('environment', 'string')
+        ;
 
         return $this->get($this->getProjectPath($project_id, 'deployments'), $resolver->resolve($parameters));
     }
